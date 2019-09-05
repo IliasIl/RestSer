998692851e7a91072791a83a4d6cbac2672a833f
Ilias <cossin90@gmail.com>
2019-09-05 20:33:09 +0300

    Created new project.

---
+++ src/main/java/com/RestSer/RestSerApplication.java
@@ -0,0 +1,13 @@
+package com.RestSer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSerApplication.class, args);
+	}
+
+}
