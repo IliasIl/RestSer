2e915913d9e7e8c81e9ddc9031dddc6c43ed7cba
Ilias <cossin90@gmail.com>
2019-09-07 14:14:23 +0300

    Отформатирован класс, отвечающий за запуск данного проекта.

--- src/main/java/com/RestSer/RestSerApplication.java
+++ src/main/java/com/RestSer/RestSerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestSerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestSerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestSerApplication.class, args);
+    }
 
 }
