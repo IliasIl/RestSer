661d1c5f174b190f895d4accd772742a7d9d60e8
Ilias <cossin90@gmail.com>
2019-09-07 21:10:34 +0300

    Вся логика из Controller перенесена в Service.

--- src/main/java/com/RestSer/controller/ClientController.java
+++ src/main/java/com/RestSer/controller/ClientController.java
@@ -17,42 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClientController {
     private final ClientService clientService;
-    private final ObjectWriter objectWriter;
-    private final ObjectWriter objectWriter2;
 
     @Autowired
-    public ClientController(ClientService clientService, ObjectMapper objectMapper) {
+    public ClientController(ClientService clientService) {
         this.clientService = clientService;
-        ObjectMapper mapper = objectMapper
-                .setConfig(objectMapper.getSerializationConfig());
-        this.objectWriter = mapper.writerWithView(Views.Balance.class);
-        this.objectWriter2 = mapper.writerWithView(Views.Full.class);
     }
 
     @PostMapping("client")
     public String createClient(@RequestBody Client client) throws JsonProcessingException {
-        String type = client.getType();
-        if (type != null && type.equalsIgnoreCase("create")) {
-            if (client.getLogin() == null) {
-                return objectWriter.writeValueAsString(new Status(2));
-            }
-            return objectWriter.writeValueAsString(clientService.createClient(client));
-        } else if (type != null && type.equalsIgnoreCase("get-balance")) {
-            if (client.getLogin() == null) {
-                return objectWriter.writeValueAsString(new Status(2));
-            }
-            int a = clientService.balanceClient(client).getResult();
-            Status status = clientService.balanceClient(client);
-            if (a == 0) {
-                return objectWriter2.writeValueAsString(status);
-            } else if (a == 3) {
-                return objectWriter.writeValueAsString(status);
-            } else {
-                return objectWriter.writeValueAsString(status);
-            }
-
-        } else {
-            return objectWriter.writeValueAsString(new Status(2));
-        }
+        return clientService.constClient(client);
     }
 }
--- src/main/java/com/RestSer/service/ClientService.java
+++ src/main/java/com/RestSer/service/ClientService.java
@@ -3,19 +3,56 @@ package com.RestSer.service;
 import com.RestSer.domain.Client;
 import com.RestSer.domain.dto.Balance;
 import com.RestSer.domain.dto.Status;
+import com.RestSer.domain.dto.Views;
 import com.RestSer.repo.ClientRepo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientService {
     private final ClientRepo clientRepo;
+    private final ObjectWriter objectWriter;
+    private final ObjectWriter objectWriter2;
+
 
     @Autowired
-    public ClientService(ClientRepo clientRepo) {
+    public ClientService(ClientRepo clientRepo, ObjectMapper objectMapper) {
         this.clientRepo = clientRepo;
+        ObjectMapper mapper = objectMapper
+                .setConfig(objectMapper.getSerializationConfig());
+        this.objectWriter = mapper.writerWithView(Views.Balance.class);
+        this.objectWriter2 = mapper.writerWithView(Views.Full.class);
     }
 
+    public String constClient(Client client) throws JsonProcessingException {
+        String type = client.getType();
+        if (type != null && type.equalsIgnoreCase("create")) {
+            if (client.getLogin() == null) {
+                return objectWriter.writeValueAsString(new Status(2));
+            }
+            return objectWriter.writeValueAsString(createClient(client));
+        } else if (type != null && type.equalsIgnoreCase("get-balance")) {
+            if (client.getLogin() == null) {
+                return objectWriter.writeValueAsString(new Status(2));
+            }
+            int a = balanceClient(client).getResult();
+            Status status = balanceClient(client);
+            String resp=objectWriter.writeValueAsString(status);
+            if (a == 0) {
+                return objectWriter2.writeValueAsString(status);
+            } else if (a == 3) {
+                return resp;
+            } else {
+                return resp;
+            }
+
+        } else {
+            return objectWriter.writeValueAsString(new Status(2));
+        }
+    }
 
     public Status createClient(Client client) {
         Client user = clientRepo.findByLogin(client.getLogin());
