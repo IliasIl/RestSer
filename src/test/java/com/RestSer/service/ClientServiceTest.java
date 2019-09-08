0b9435dba4c9706b71920218e8a5d62c1d22a81e
Ilias <cossin90@gmail.com>
2019-09-08 20:51:43 +0300

    Добавлены unit-тесты.

--- src/test/java/com/RestSer/service/ClientServiceTest.java
+++ src/test/java/com/RestSer/service/ClientServiceTest.java
@@ -1,5 +1,65 @@
-import static org.junit.Assert.*;
+package com.RestSer.service;
 
+import com.RestSer.domain.Client;
+import com.RestSer.domain.dto.Status;
+import com.RestSer.repo.ClientRepo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ClientServiceTest {
 
+    @Autowired
+    private ClientService clientService;
+
+    @MockBean
+    private ClientRepo clientRepo;
+
+    @Test
+    public void constClient() throws JsonProcessingException {
+        /*Проверим, что у пустого клиента не будет осуществлен поиск в БД,
+          а также добавление. Возврат будет с кодом 2.*/
+        Client client = new Client();
+        String str = clientService.constClient(client);
+        Mockito.verify(clientRepo, Mockito.times(0)).findByLogin(ArgumentMatchers.anyString());
+        Mockito.verify(clientRepo, Mockito.times(0)).save(ArgumentMatchers.any(Client.class));
+        Assert.assertThat(str, Matchers.containsString("2"));
+    }
+
+    @Test
+    public void constClient2() throws JsonProcessingException {
+        /*Проверим, что в случае заполненного логина и типа будет вызван поиск в БД.*/
+        Client client = new Client();
+        client.setType("create");
+        client.setLogin("Il");
+        Mockito.doReturn(new Client()).when(clientRepo).findByLogin(client.getLogin());
+        String str = clientService.constClient(client);
+        Mockito.verify(clientRepo, Mockito.times(1)).findByLogin(ArgumentMatchers.anyString());
+        /*Проверим, что в случае повторного сохранения
+          клиента с одним и тем же логином не будет происходить сохраниния в БД.*/
+        Assert.assertThat(str, Matchers.containsString("1"));
+        Mockito.verify(clientRepo, Mockito.times(0)).save(ArgumentMatchers.any(Client.class));
+    }
+
+    @Test
+    public void balanceClient() {
+        /*Проверим, что будет код 3 в случае если такого пользователя нет в БД.*/
+        Client client = new Client();
+        Status status = clientService.balanceClient(client);
+        Mockito.verify(clientRepo, Mockito.times(1))
+                .findByLogin(null);
+        Mockito.verify(clientRepo, Mockito.times(0))
+                .findByLoginAndPassword(null, null);
+        Assert.assertThat(String.valueOf(status.getResult()), Matchers.containsString("3"));
+    }
 }
